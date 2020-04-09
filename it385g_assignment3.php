402e9e16f0b89426782ca6460e8ef9f5fad7f6b7
michaelArvidsson <michael.arvidsson@gmail.com>
2020-04-09 16:47:08 +0200

    fixed webservice error and cleaned up code

--- it385g_assignment3.php
+++ it385g_assignment3.php
@@ -1,8 +1,8 @@
 <html lang="en" dir="ltr">
 <head>
-    <meta charset="utf-8">
-    <title>Big trucks</title>
-<style>
+  <meta charset="utf-8">
+  <title>Big trucks</title>
+  <style>
     * {
         font-family: Courier, monospace;
         font-weight:bold;
@@ -34,48 +34,27 @@
         padding:10px;
     }
 
-</style>
+    </style>
 
 <body>
 <pre>
 <h1 id='head'>Big Trucks information database</h1>    
-<form method='post' action='respond_it385g_assignment3.php'>                       
-<?php                                                                                                                                                 
- 
-$url="https://wwwlab.iit.his.se/gush/XMLAPI/vehiclesservice/manufacturer/";
-$jsontext = file_get_contents($url);
-$arr = json_decode($jsontext);
-echo "<div id='f_body'>";
-echo "<label>Select Manufacturer </label><select name='country'>";
-foreach($arr as $manufacturer){
-    echo "<option value='".$manufacturer[1]."' >".$manufacturer[0]."</option>";
-}
-echo "</select>";
-/*New array with only countries
-$newarray = array();
-foreach ($trucks as $truck) {
-  if(isset($truck[2])){
-     $newarray[] =  $truck[2];
+<form method='post' action='respond_it385g_assignment3.php'>
+
+<?php                                                                                                                                                  
+  $url="https://wwwlab.iit.his.se/gush/XMLAPI/vehiclesservice/manufacturer/";
+  $jsontext = file_get_contents($url);
+  $arr = json_decode($jsontext);
+  echo "<div id='f_body'>";
+  echo "<label>Select Manufacturer </label><select name='country'>";
+  foreach($arr as $manufacturer){
+      echo "<option value='".$manufacturer[1]."' >".$manufacturer[0]."</option>";
   }
-}
-//Remove duplicates and sort array
-$newarray = array_unique($newarray);
-sort($newarray);
-// Test $newarray
-//print_r($newarray);
-echo "<h1 id='head'>Big Trucks information database</h1>";
-echo "<form method='post' action='respond_it385g_assignment2.php'>";
-echo "<div id='f_body'>";
-echo "<label>Select country </label><select name='country'>";
-foreach ($newarray as $country) {
-    echo "<option value='".$country."' >".$country."</option>";
-}*/
-//echo "</select>";
-echo "<input style='margin-left:10px'; type='submit' name='submitbutton' value='Show result'>";
-//echo "</div>";
-//echo "</form>";
+  echo "</select>";
+  echo "<input style='margin-left:10px'; type='submit' name='submitbutton' value='Show result'>";
+  echo "</div>";
     
-?>  
+  ?>  
 </form>                          
 </pre>
 </body>
--- respond_it385g_assignment3.php
+++ respond_it385g_assignment3.php
@@ -1,117 +1,97 @@
 <html lang="en" dir="ltr">
+
 <head>
-    <meta charset="utf-8">
-    <title>Big trucks</title>
-<style>
+  <meta charset="utf-8">
+  <title>Big trucks</title>
+  <style>
     * {
-        font-family: Courier, monospace;
-        font-weight:bold;
+      font-family: Courier, monospace;
+      font-weight: bold;
     }
+
     body {
-        background-color:#5c8261;
+      background-color: #5c8261;
     }
-    table{
-        margin:auto;
-        background-color:lightgrey;
-    }
+
+    table,
     td {
-        padding:0px;
-        
+      border: 1px solid black;
+      margin: auto;
+      background-color: lightgrey;
     }
-    button {
-        width: 50px;
-        color:black;
-        text-decoration:none;
-        transition: 0.8s;
-        padding:5px;
-        margin: auto;
-        margin-bottom:10px;
-        font-weight:bold;
+
+    table {
+      box-shadow: 2px 2px 4px 2px black;
+
     }
-    button:hover {
-        box-shadow: 0px 0px 4px 3px white;
-    }
-    #tbl {
-        box-shadow: 2px 2px 4px 2px;
-    }
+
     #selection {
-        Background-color: black;
-        width:350px;
-        border: 2px solid white;
-        margin:auto;
-        margin-bottom: 20px;
-        color: white;
-        padding: 5px;
-        text-align:center;
-        font-weight:bold;
-        font-size: 120%;
-        box-shadow: 2px 2px 4px 2px black;
+      Background-color: black;
+      width: 350px;
+      border: 2px solid white;
+      margin: auto;
+      margin-bottom: 20px;
+      color: white;
+      padding: 5px;
+      text-align: center;
+      font-weight: bold;
+      font-size: 120%;
+      box-shadow: 2px 2px 4px 2px black;
     }
+
     #caption {
-        background-color: black;
-        color:coral;
-        font-size: 200%;
-        font-weight: 700;
-        letter-spacing: 10px;
-        text-align:center;
-        text-transform: uppercase;
-        padding:10px;
+      background-color: black;
+      color: coral;
+      font-size: 200%;
+      font-weight: 700;
+      letter-spacing: 10px;
+      text-align: center;
+      text-transform: uppercase;
+      padding: 10px;
     }
+
     #head {
-        Background-color: black;
-        color: coral;
-        padding: 15px;
-        font-size: 120%;
+      Background-color: black;
+      color: coral;
+      padding: 15px;
+      font-size: 120%;
     }
+
     #sub {
-        font-weight: bold;
-        text-align: center;
-        padding: 15px;
+      font-weight: bold;
+      text-align: center;
+      padding: 15px;
     }
-</style>
+  </style>
+
 <body>
 <pre>
-<table id='tbl' border='2px'>
+<table>
 <?php
 //Check form value
-if(isset($_POST['country'])){
-    $country=$_POST['country'];
-}else{
-    $country="USA";
+if (isset($_POST['country'])) {
+  $country = $_POST['country'];
+} else {
+  $country = "USA";
 }
+// Get array from webservice
+$url = "https://wwwlab.iit.his.se/gush/XMLAPI/vehiclesservice/vehicles/?country=" . $country;
+$jsontext = file_get_contents($url);
+$arr = json_decode($jsontext);
 
-/*check if value is missing
-if (empty($incountry)) {
-        echo "<h1 id='caption'>Big Trucks information database</h1>";
-        echo "<div id='selection'>";
-        echo "<p>Your selection is empty</p>";
-        echo "<button type='button' name='backbutton'><a href='it385g_assignment2.php' >Back</a></button>";
-        echo "</div>";
-// kör tabellen      
-}else{*/
-    $url="https://wwwlab.iit.his.se/gush/XMLAPI/vehiclesservice/vehicles/?country=".$country;
-    $jsontext = file_get_contents($url);
-    $arr = json_decode($jsontext);
-    
-   echo "<h1 id='caption'>Big Trucks information database</h1>";
-    //echo "<div id='selection'>";
-      //  echo "<p>Your selection: $country</p>";
-    //echo "</div>";
-    
-    echo "<tr><th id='head'>Manufacturer</th><th id='head'>Drive shafts</th><th id='head'>Horsepowers</th></tr>";
-    foreach ($arr[0] as $trucks) {
-        //if($country[2]==$incountry){  
-        echo "<tr>";
-        //echo "<td id='sub'>" . $country. "</td>";  
-        echo "<td id='sub'>" . $trucks[0]. "</td>";
-        echo "<td id='sub'>" . $trucks[1] . "</td>";
-        echo "<td id='sub'>" . $trucks[2] . "</td>";
-    }
+echo "<h1 id='caption'>Big Trucks information database</h1>";
+// run table 
+echo "<tr><th id='head'>Manufacturer</th><th id='head'>Drive shafts</th><th id='head'>Horsepowers</th></tr>";
+foreach ($arr[0] as $trucks) {
+  echo "<tr>";
+  echo "<td id='sub'>" . $trucks[0] . "</td>";
+  echo "<td id='sub'>" . $trucks[1] . "</td>";
+  echo "<td id='sub'>" . $trucks[2] . "</td>";
+}
 // Test POST
 //print_r($_POST);
 ?>
 </table>
-
 </pre>
 </body>
 </html>
